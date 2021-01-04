601936cd284afcf0dbb79c87ad60578da37a3a04
Sayed Sokar <64536541+SayedSokar@users.noreply.github.com>
2021-01-04 04:02:22 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,6 +1,8 @@
 //-------------- varibales defination----------------//
-let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip='
-let apiKey = ',us&appid=cc5bf4d3742aa3ac627686fffec215c2';
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip='
+const apiKey = '&appid=cc5bf4d3742aa3ac627686fffec215c2';
+
+
 const resultDiv = document.querySelector('.resContainer')
 console.log(resultDiv)
 const fragment = document.createDocumentFragment();
@@ -16,57 +18,40 @@ function createIndItem(itemTxt, elementType, attName, attValue, attName1, attVal
   }
     return newElement;
 };
-//----------- Create -----------//
+
 //------------performAction-------------------------------//
 function performAction(){
   const zip = document.getElementById('zip').value;
+  const cunteryid = document.querySelector('#counteryid').value
   const feelings = document.getElementById('feelings').value
-  getwez(baseURL, zip, apiKey)
+  getwez(baseURL, zip, cunteryid, apiKey)
     .then(function (data) {
       console.log('this is the data from weather web',data)
-      // if (typeof data != "undefined") {
-
-        
+             
           postData('/addWezData', { temp: data.main.temp, cuntery: data.sys.country, city: data.name, dt: data.dt, feeling: feelings })
-      console.log('post done')
-        //.then(function (newData) {
+              
           updateUI()
-        //})
-          
-      
-        
-      // } else {
-
-        //console.log('No POST data to the server');
-        
-      // }
-      //console.log(data)
-      
     })
-    // .then(
-    //   updateUI()
-    // )
+    
 }
-//------------------------------------------------//
-const getwez = async (baseURL, zip, key) => {
-  //console.log(baseURL+zip+key)
-  const res = await fetch(baseURL + zip + key)
-  
-    try {
+//----------------------Get the weather Data----------------//
+const getwez = async (baseURL, zip, cunteryid, key) => {
+    const res = await fetch(baseURL + zip + ',' + cunteryid + key)
+      try {
       const data = await res.json();
       if (res.ok) {
         document.getElementById('msg').innerText =''
         return data
         
       } else {
-        document.getElementById('msg').innerText = 'Zip code not found,Please Check Zip Code and try again'
+        document.getElementById('msg').innerText = 'Zip code not found,Please Check Zip Code and try again' // play msg for the not found ZIP
         throw new Error('Zip code not found,Please Check Zip Code and try again');
         
       }
     } catch (error) {
-      //document.getElementById('msg').innerHTML = 'zip code not found please enter a valid zip code'
+      
       console.log(error);
-      // appropriately handle the error
+      
     }
 }
 //----------------------------------------------//
@@ -99,10 +84,8 @@ const updateUI = async () => {
     const existline = document.querySelectorAll('.result')
     console.log(existline.length)
     
-    
-    if (allData.length > 0) {
-      for (let i = existline.length; i < allData.length;i++) {
-        //console.log(allData.length-1)
+          for (let i = existline.length; i < allData.length;i++) {
+        
         let cuntDiv = createIndItem('', 'div', 'class', 'result','','') // to create cuntainer Div
         for (const [key, value] of Object.entries(allData[i])) {
           let itemTxt = value;
@@ -112,28 +95,9 @@ const updateUI = async () => {
         
         cuntDiv.appendChild(fragment);
         titleDiv.after(cuntDiv)
-        //resultDiv.appendChild(cuntDiv);
-
-
-      //   document.getElementById('date').innerHTML = allData[allData.length-1].dt;
-      //   document.getElementById('cuntery').innerHTML = allData[allData.length-1].cuntery;
-      // document.getElementById('city').innerHTML = allData[allData.length-1].city;
-      // document.getElementById('temp').innerHTML = allData[allData.length-1].temp;
-      // document.getElementById('content').innerHTML = allData[allData.length-1].feeling;
-      // document.getElementById('date1').innerHTML = allData[allData.length-2].dt;
-      //   document.getElementById('cuntery1').innerHTML = allData[allData.length-2].cuntery;
-      // document.getElementById('city1').innerHTML = allData[allData.length-2].city;
-      // document.getElementById('temp1').innerHTML = allData[allData.length-2].temp;
-      // document.getElementById('content1').innerHTML = allData[allData.length-2].feeling;
+              
       }
       
-    } else {
-      console.log('length is zero')
-      updateUI()
-      //performAction()
-      //console.log(res.body.json())
-    }
-
   }catch(error){
     console.log("error", error);
   }
